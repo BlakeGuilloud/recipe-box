15e6c2d439190413771967abbee217d516430a0d
Forrest Guilloud <fbguillo@gmail.com>
2017-01-02 13:00:29 -0500

    mocked validation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { SendSuccess, SendError } from 'tael';
 import Shuttle from './shuttle';
 import * as AuthService from '../services/auth.service';
+import * as UserService from '../services/user.service';
 
 export function login(req, res) {
   return Promise.resolve(Shuttle.liftRequest(req))
@@ -9,15 +10,9 @@ export function login(req, res) {
     .catch(SendError(res));
 }
 
-export function logout(req, res) {
-  Promise.resolve()
-    .then(() => req.logout())
-    .then(SendSuccess(res))
-    .catch(SendError(res));
-}
-
 export function registerUser(req, res) {
   return Promise.resolve(Shuttle.liftRequest(req))
+    .then(UserService.findByUserName)
     .then(Shuttle.liftFunction(AuthService.registerUser, 'data'))
     .then(SendSuccess(res))
     .catch(SendError(res));
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,1 +1,17 @@
 import User from '../models/user.model';
+import { ConflictingRecordError } from 'tael';
+
+export async function findByUserName(shuttle) {
+  const username = shuttle.data.username;
+  let user;
+
+  try {
+    user = await User.findOne({ username });
+  } catch (err) {
+    return Promise.reject(err);
+  }
+
+  if (user) return Promise.reject(new ConflictingRecordError(`A user with username: '${username}' already exists`))
+
+  return Promise.resolve(shuttle);
+}
